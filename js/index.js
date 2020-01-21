f361378516601a0bbce51d0d4cfc842b1a6f5ecb
Mark Sayers <marksayers46@icloud.com>
2020-01-20 20:33:12 -0600

    Added eventListener to buttons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //get all .nav a elements
 const newNav = document.querySelectorAll('.nav a');
 //console.log(newNav);
-//add event listener on multiple elements
+//add eventListener for multiple elements
 newNav.forEach(navigation => {
     navigation.addEventListener('click', (event) => {
         event.target.style.color = 'blue'
@@ -11,21 +11,26 @@ newNav.forEach(navigation => {
 //get header section topImg
 const topContent = document.querySelector('.topImg');
 //console.log(topContent);
-//add event listener mouseenter
-topContent.addEventListener('mouseenter', () => {
+//add eventListener mouseenter
 //good practice pairing transform + transition for smooth transition
+topContent.addEventListener('mouseenter', () => {
     topContent.style.transform = 'scale(1.1)';
     topContent.style.transition = 'all 1.3s';
 });
-//add event listener mouseleave
+//add eventListener mouseleave
 topContent.addEventListener('mouseleave', () =>{
     topContent.style.transform = 'scale(1)';
     topContent.style.transition = 'all 1.3s';
 });
 
 //get buttons
-const bottomButton = document.querySelectorAll('btn');
-//console.log(bottomButton);
-
+const bottomButtons = document.querySelectorAll('.btn');
+//console.log(bottomButtons);
+//add eventListener for multiple elements
+bottomButtons.forEach(buttons => {
+    buttons.addEventListener('click', (event) => {
+        event.target.style.color = 'red';
+    });
+});
 
 
